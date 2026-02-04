48c0070985f6202730b0e4c766861d5e12e288c5
Naresh K <naresh.kalakanti@gmail.com>
2026-02-04 08:24:59 +0000

    chore: initialize package.json with express dependency and basic scripts

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM --plaform=linux/amd64 node:latest
+WORKDIR /app
+COPY . /app
+RUN npm install
+EXPOSE 3000
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("We are on ECS demo")
+});
+
+app.get('/health',()=>{
+  res.status(200).send('App is up and running')
+})
+
+app.listen(port, () => {
+  console.log(`Server is  listening on port http://localhost:${port}`)
+});
